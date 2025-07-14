f538bccc6137b751e181741add7c0e3baa83138b
ahmad kadri <61470487+ahmadkadri978@users.noreply.github.com>
2025-07-14 15:40:00 +0200

    Update BookServiceImpl.java

--- src/main/java/kadri/Digital/Library/Management/System/service/BookServiceImpl.java
+++ src/main/java/kadri/Digital/Library/Management/System/service/BookServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
